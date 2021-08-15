30306d92520f44eb0442a440d6316d0304209767
Venkatesh Prasad B L <venkpras92@gmail.com>
2021-08-15 16:19:05 +0530

    New code

--- new.py
+++ new.py
@@ -1,7 +1,3 @@
-import numpy as numpy
-# 2x2 matrix with 1's on main diagonal
-obj1 = numpy.eye(2, dtype = float)
-print("Matrix : \n", obj1)
-# matrix with Row=2 Column=3 and diagonal=1
-obj2 = numpy.eye(3, 3, k = -2)
-print("\nMatrix : \n", obj2)
+import numpy as np
+
+a= np.arange(20).reshape(5,2,2)
